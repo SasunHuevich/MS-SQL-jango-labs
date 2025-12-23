de3240d786329c71fdc4073282e0e6b69a6450a4
Ivan Petrov <ivanp6174@gmail.com>
2025-12-24 02:54:22 +0800

    fix: build and run scripts

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 
-echo "Building Rust binary..."
-docker-compose -f docker-compose.build.yaml run --rm builder
+#echo "Building Rust binary..."
+#docker-compose -f docker-compose.build.yaml run --rm builder
 
 echo "Stopping old containers..."
 docker compose down
@@ -10,10 +10,16 @@ docker compose down
 echo "Building Docker image..."
 docker compose build
 
-echo "Starting containers..."
-docker compose up -d
+echo "Starting db container..."
+docker compose up -d db
+
+echo "Sleep 5..."
+sleep 5
 
 echo "Init db..."
 docker exec -it db bash /docker-entrypoint-initdb.d/init_db.sh
+
+echo "Starting back container..."
+docker compose up -d back
 
 echo "Done!"
