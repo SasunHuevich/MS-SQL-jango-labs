126ab67cdf7bd42aead410df94dcffafc7e7986c
11Samedi <ivanp6174@gmail.com>
2025-12-25 01:09:22 +0800

    refactor: all_seeds in one func

--- src/main.rs
+++ src/main.rs
@@ -9,42 +9,7 @@ async fn main() -> Result<(), sqlx::Error> {
 
     let db_pool = db::init_db().await;
 
-    let users: i32 = 700_000;
-    let pictures: i32 = 30_000;
-    let traders: i32 = 20;
-    let items: i32 = 50_000;
-    let quests: i32 = 1_000;
-    let quest_rewards: i32 = 3_000;
-    let maps: i32 = 10;
-    let comments: i32 = 150_000;
-    let map_markers: i32 = 100 * maps;
-    let quest_refs: i32 = quests / 2;
-    let completed_quests: i32 = users * quests / 4000;
-    let quest_maps: i32 = quests * maps / 6;
-
-
-    macro_rules! try_seed {
-        ($name:expr, $func:expr $(, $args:expr)*) => {
-            if let Err(e) = $func(&db_pool $(, $args)*).await {
-                eprintln!("Seed failed for {}: {e}", $name);
-            } else {
-                println!("Seeding {} done", $name);
-            }
-        };
-    }
-
-    try_seed!("users", seed::seed_users, users);
-    try_seed!("pictures", seed::seed_pictures, pictures);
-    try_seed!("traders", seed::seed_traders, traders, pictures);
-    try_seed!("items", seed::seed_items, items, pictures);
-    try_seed!("quests", seed::seed_quests, quests, pictures, traders);
-    try_seed!("quest_rewards", seed::seed_quest_rewards, quest_rewards, quests, items, traders);
-    try_seed!("maps", seed::seed_maps, maps, pictures);
-    try_seed!("comments", seed::seed_comments, comments, users, quests);
-    try_seed!("map_markers", seed::seed_map_markers, map_markers, maps, pictures);
-    try_seed!("quest_refs", seed::seed_quest_refs, quest_refs, quests);
-    try_seed!("user_complete_quests", seed::seed_user_complete_quests, completed_quests, users, quests);
-    try_seed!("quest_map", seed::seed_quest_map, quest_maps, quests, maps);
+    seed::seeds_generate(&db_pool).await;
 
     Ok(())
 }
--- src/seed.rs
+++ src/seed.rs
@@ -707,3 +707,42 @@ pub async fn seed_quest_map(
 
     Ok(())
 }
+
+pub async fn seeds_generate(pool: &Pool<Mssql>) {
+    let users: i32 = 700_000;
+    let pictures: i32 = 30_000;
+    let traders: i32 = 20;
+    let items: i32 = 50_000;
+    let quests: i32 = 1_000;
+    let quest_rewards: i32 = 3_000;
+    let maps: i32 = 10;
+    let comments: i32 = 150_000;
+    let map_markers: i32 = 100 * maps;
+    let quest_refs: i32 = quests / 2;
+    let completed_quests: i32 = users * quests / 4000;
+    let quest_maps: i32 = quests * maps / 6;
+
+
+    macro_rules! try_seed {
+        ($name:expr, $func:expr $(, $args:expr)*) => {
+            if let Err(e) = $func(&pool $(, $args)*).await {
+                eprintln!("Seed failed for {}: {e}", $name);
+            } else {
+                println!("Seeding {} done", $name);
+            }
+        };
+    }
+
+    try_seed!("users", seed_users, users);
+    try_seed!("pictures", seed_pictures, pictures);
+    try_seed!("traders", seed_traders, traders, pictures);
+    try_seed!("items", seed_items, items, pictures);
+    try_seed!("quests", seed_quests, quests, pictures, traders);
+    try_seed!("quest_rewards", seed_quest_rewards, quest_rewards, quests, items, traders);
+    try_seed!("maps", seed_maps, maps, pictures);
+    try_seed!("comments", seed_comments, comments, users, quests);
+    try_seed!("map_markers", seed_map_markers, map_markers, maps, pictures);
+    try_seed!("quest_refs", seed_quest_refs, quest_refs, quests);
+    try_seed!("user_complete_quests", seed_user_complete_quests, completed_quests, users, quests);
+    try_seed!("quest_map", seed_quest_map, quest_maps, quests, maps);
+}
